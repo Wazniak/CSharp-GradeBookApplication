35e641b863c5c99bb4c4ab55aec5cb522789f868
Wazniak <kontakt@karolwozniak.com>
2019-01-28 23:02:21 +0100

    Exercise 7 - final

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,10 +13,12 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
@@ -109,21 +111,33 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var currentGPA = 0;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    currentGPA = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    currentGPA = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    currentGPA = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    currentGPA = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    currentGPA = 0;
+                    break;
             }
 
-            return 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                currentGPA++;
+            }
+
+            return currentGPA;
         }
 
         public virtual void CalculateStatistics()
